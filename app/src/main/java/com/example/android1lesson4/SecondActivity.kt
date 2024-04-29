15171a4c4addb4fb1e3bbf82afd2f655965a80c9
Isakov09 <166392908+Isakov09@users.noreply.github.com>
2024-04-29 22:35:26 +0600

    Initial commit

---
+++ app/src/main/java/com/example/android1lesson4/SecondActivity.kt
@@ -0,0 +1,16 @@
+package com.example.android1lesson4
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sekond)
+
+
+    }
+}
